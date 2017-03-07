7760381f540961a54dd326d7d2cae0d1422d8467
Sunit Kumar Adhikary <sunit1409@users.noreply.github.com>
2017-03-07 17:50:02 +0530

    Add files via upload

--- as_per_question.m
+++ as_per_question.m
@@ -4,12 +4,9 @@ function [ value ] = as_per_question( n )
     if(n > 1 )
         initial = 1 + n ;
     end
-    if(n == 1 )
-        value = 1 ;
-    end
-    if(n == 2 )
-        value = initial ;
-    end
+                                    if(n == 1 )
+                                        value = 1 ;
+                                    end
    add_value  =  0 ;
     if(n > 2)
        
@@ -18,7 +15,7 @@ function [ value ] = as_per_question( n )
                 if(i-1 > s  )
                     break      % That is when it is not possible to place s coins between  i + 1 beggars such that i-1 beggars gets at least 1 coint
                 end
-                add_value = add_value + nchoosek(s+1,i) ;
+                add_value = add_value + nchoosek(s+1,i) ;  %distributing s coins to i+1 beggars so that i-1 beggars get more than 1 coin   so that consecuitive 0 can be avoided
             end 
     end
     value = initial + add_value;
---
+++ better.m
@@ -0,0 +1,15 @@
+t = input('');
+input_data = zeros(t,1);
+result = zeros(t,1);
+
+for i = 1 : t
+    input_data(i) = input('');
+    if((input_data(i)>=1 ) && (input_data(i) <=12))
+        result(i) = mod(as_per_question(input_data(i)) , power(10,9) + 7  );
+ 
+    else
+        result(i) = fast_exp(377/233 ,input_data(i)-11 , power(10,9) + 7) * 233;
+    end
+end
+
+result
